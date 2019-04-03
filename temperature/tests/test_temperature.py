29ad35f78b9e8edfcf87b9355401c31040ad3585
luknuk <github@ldmartin.com>
2019-04-03 13:44:24 +1000

    Added private test function to decrease verbosity

--- temperature/tests/test_temperature.py
+++ temperature/tests/test_temperature.py
@@ -6,15 +6,37 @@ import sys
 sys.path.append('../')
 import temperature
 
+from decimal import *
+
+
+'''
+Because floats introduce miniscule inaccuracies,
+it's important that we round to two places when comparing.
+'''
+TWOPLACES = Decimal(10) ** -2 #same as Decimal('0.01')
+def _round(rawTemperature):
+    return Decimal(rawTemperature).quantize(TWOPLACES)
+
 class TestTemperatureMethods(unittest.TestCase):
+
+    
 
     # Each test, constructs new temperature object        
     def setUp(self):
         self.temperature = temperature.Temperature()
 
+
+
     def test__celsiusToKelvin(self):
-        self.assertEqual(373.15, self.temperature._celsiusToKelvin(100))
-        self.assertEqual(173.15, self.temperature._celsiusToKelvin(-100))
+
+        self.assertEqual(100, _round(100))
+
+        # Reduces verbosity of code
+        def test(C, K):
+            self.assertEqual(Decimal(C), _round(self.temperature._celsiusToKelvin(K)))
+        
+        test(373.15, 100)
+        test(173.15, -100)
 
 if __name__ == '__main__':
     # if test is being run directly
